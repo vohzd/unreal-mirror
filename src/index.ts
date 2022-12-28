3d57ffb4a8586ee274653610ad575e8aa5a3566e
vohzd <github@vohzd.com>
2022-12-28 16:14:59 +0000

    Add in new models for different contentTypes

--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,29 @@
-import { convertYamlToJson } from "./utils/convertYamlToJson.js";
-import { gatherSourceFileDirectories } from "./utils/gatherSourceFileDirectories.js";
 
-import { Map } from "./db/models/map.js";
-import { sequelize } from "./db/sequelize.js";
+import { createIndex } from "./utils/createIndex.js";
+import { downloadAll } from "./utils/download.js";
 console.log(`
 *******************************
 ***** UNREAL FILE FINDER ******
 *******************************
 `);
 
-
 (async () => {
 
-  // ensure our db exists
-  await sequelize.sync();
+  const arg = process.argv[2]
 
-  // get a list of all our directories
-  const directories = await gatherSourceFileDirectories()
+  if (arg === "--createIndex") {
+    await createIndex();
+  }
+  if (arg === "--downloadAll") {
+    console.log("downloading all...");
+    await downloadAll();
+
+  }
 
 
-  // convert yaml files in these dirs to a JS object (in memory)
-  const metadata = [];
 
-  directories.forEach(async (path) => {
-    console.log(`Reading from ${path}`);
-    const json = convertYamlToJson(path)
 
-    if (json.contentType === "MAP") {
-      metadata.push(json)
-    }
 
-  })
-
-  console.log("PARSED METADATA!");
-  console.log(metadata);
-  // write to sqlite db
-
-  Map.bulkCreate(metadata);
-
-  // created everything, now dump them
-  console.log("bulk create finished...");
-  console.log("reading..");
-  const maps = await Map.findAll();
-  console.log(JSON.stringify(maps));
 
 })();
 
